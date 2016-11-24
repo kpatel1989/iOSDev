b25ce8b7090ad830e330876f5e0c6047cc1d8e1c
Kartik Patel <katk.1989@gmail.com>
2016-11-24 10:26:30 -0500

    assignment

---
+++ KartikAssignment/KartikAssignment/KartikQuestion5TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KartikQuestion5TableViewCell.swift
+//  KartikAssignment
+//
+//  Created by macadmin on 2016-11-24.
+//  Copyright © 2016 LambtonCollege. All rights reserved.
+//
+
+import UIKit
+
+class KartikQuestion5TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
