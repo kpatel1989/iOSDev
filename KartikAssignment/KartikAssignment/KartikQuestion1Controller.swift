76b340a67d9e1de0a930208f3f1c464793ab8bf3
Kartik Patel <katk.1989@gmail.com>
2016-11-25 08:25:06 -0500

    assignment

--- KartikAssignment/KartikAssignment/KartikQuestion1Controller.swift
+++ KartikAssignment/KartikAssignment/KartikQuestion1Controller.swift
@@ -12,16 +12,15 @@ class KartikQuestion1Controller: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //QuestionBank.getNextQuestion() -> []
     }
     
-
     /*
     // MARK: - Navigation
 
---
+++ KartikAssignment/KartikAssignment/QuestionBank.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionBank.swift
+//  KartikAssignment
+//
+//  Created by macadmin on 2016-11-25.
+//  Copyright © 2016 LambtonCollege. All rights reserved.
+//
+
+import Foundation
+
+class QuestionBank {
+    var randomQuestions: [Int]!
+    
+    init() {
+        //Bundle
+        //genereate Random numbers to display
+        
+    }
+    
+    // random numbers function public save to randomQuestions
+    
+    // get Next question // returns next index of the randomQuestion array
+}
